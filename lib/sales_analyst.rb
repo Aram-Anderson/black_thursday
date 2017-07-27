0737761c1bb3af1c1d9a98f3f11ed3b6a5d6b2d6
JunePaloma <kborkoski@gmail.com>
2017-07-27 15:18:46 -0600

    Add support methods for high achiving merchant method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -11,4 +11,8 @@ class SalesAnalyst
   def average_items_per_merchant_standard_deviation
     @engine.average_items_per_merchant_standard_deviation
   end
+
+  def merchants_with_highest_item_count
+    @engine.merchants_with_highest_item_count 
+  end
 end
